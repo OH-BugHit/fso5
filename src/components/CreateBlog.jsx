f4cb7a09c2ee92b74bf6cb822b29198a3412ad37
Olli Hilke <hilkolli@student.uef.fi>
2023-08-24 21:02:09 +0300

    5.16 done

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -1,60 +1,63 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 const CreateBlog = ({ createBlog }) => {
 
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    const [url, setUrl] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
-    const handleAddBlog = (event) => {
-        event.preventDefault()
-        createBlog({
-            title: title,
-            author: author,
-            url: url
-        })
+  const handleAddBlog = (event) => {
+    event.preventDefault()
+    createBlog({
+      title: title,
+      author: author,
+      url: url
+    })
 
-        setTitle('')
-        setAuthor('')
-        setUrl('')
-    }
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
 
-    return (
+  return (
+    <div>
+      <h2>create new</h2>
+
+      <form onSubmit={handleAddBlog}>
         <div>
-            <h2>create new</h2>
-
-            <form onSubmit={handleAddBlog}>
-                <div>
-                    title:
-                    <input
-                        type="text"
-                        value={title}
-                        onChange={({ target }) => setTitle(target.value)}
-                    />
-                </div>
-                <div>
-                    author:
-                    <input
-                        type="text"
-                        value={author}
-                        onChange={({ target }) => setAuthor(target.value)}
-                    />
-
-                </div>
-                <div>
-                    url:
-                    <input
-                        type="text"
-                        value={url}
-                        onChange={({ target }) => setUrl(target.value)}
-                    />
-                </div>
-                <div>
-                    <button className='createButton' type='submit'>create</button>
-                </div>
-            </form>
+          title:
+          <input
+            type="text"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+            placeholder='type title here'
+          />
         </div>
-    )
+        <div>
+          author:
+          <input
+            type="text"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+            placeholder='type author here'
+          />
+
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+            placeholder='type url here'
+          />
+        </div>
+        <div>
+          <button className='createButton' type='submit'>create</button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default CreateBlog
