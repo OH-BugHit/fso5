be0baa12d4f95e360366c7a98fb68df5a4a9ef76
Olli Hilke <hilkolli@student.uef.fi>
2023-08-25 10:54:45 +0300

    5.17 done

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
--- src/components/CreateBlog.test.js
+++ src/components/CreateBlog.test.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
